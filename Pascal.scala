2a9a2da1a9c0418149f5b60a6f3590f708ebec9c
IrinaLen <ia.len@mail.ru>
2016-03-12 20:27:11 +0300

    pascal

---
+++ Pascal.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by irik on 09.03.16.
+  */
+object Pascal {
+  def main(args: Array[String]) {
+    def pascal(c: Int, r: Int): Int = {
+      if (r < c) -1
+      else if (r == c || c == 0) 1
+      else pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
+    pascal (2, 10)
+  }
+}
