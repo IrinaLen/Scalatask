70a9c20486464ea1758e227a0a40d46c68221590
IrinaLen <ia.len@mail.ru>
2016-03-12 20:25:48 +0300

    filt2

---
+++ filt2.scala
@@ -0,0 +1,24 @@
+/**
+  * Created by irik on 10.03.16.
+  */
+object filt2 {
+  def main(args: Array[String]) {
+    def fn1(x: Int): Boolean = {
+      if (x > 10) true
+      else false
+    }
+
+    def filt(ls: List[Int], fn: Int => Boolean): List[Int] = {
+      def sort(l: List[Int], tot: List[Int]): List[Int] = {
+        if (l.isEmpty) tot
+        else {
+          if (fn(l.last)) sort(l.init,l.last::tot)
+          else sort(l.init, tot)
+        }
+      }
+      sort(ls, List())
+    }
+
+    filt(List(7, 9, 0, 4, 21, 290), fn1)
+  }
+}
