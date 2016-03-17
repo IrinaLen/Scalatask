7e0dee9c060db7b7a391affe562f021739387940
IrinaLen <ia.len@mail.ru>
2016-03-17 22:56:52 +0300

    change

--- filt.scala
+++ filt.scala
@@ -7,8 +7,8 @@ object filt {
       def sort(l: List[Int], tot: List[Int]): List[Int] = {
         if (l.isEmpty) tot
         else {
-          if (l.last <= 45 && (l.last % 7) == 0) sort(l.init,l.last::tot)
-          else sort(l.init, tot)
+          if (l.head <= 45 && (l.head % 7) == 0) sort(l.tail,tot:::l.head::Nil)
+          else sort(l.tail, tot)
         }
       }
       sort(ls, List())
