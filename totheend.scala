b52799f5be331b17f4f5c1fc920ecbbb046f56e9
IrinaLen <ia.len@mail.ru>
2016-03-17 22:57:56 +0300

    change end

--- totheend.scala
+++ totheend.scala
@@ -5,10 +5,10 @@ object totheend {
   def main(args: Array[String]) {
     def inend(l: List[Any], a: Any): List[Any] = {
       def add(ls: List[Any], el: List[Any]): List[Any] = {
-        if (ls.isEmpty) el:::a::Nil
+        if (ls.isEmpty) el
         else add(ls.init, ls.last::el)
       }
-      add(l, List())
+      add(l, a::List())
     }
     inend(List(1, 2, "Len"), "Ira")
   }
